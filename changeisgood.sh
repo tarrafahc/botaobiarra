5a0913ab3ed1107bd27d2084796b3508ec6e11aa
tarrafabot <tarrafahc@gmail.com>
2017-09-23 19:33:43 -0300

    aberto

--- changeisgood.sh
+++ changeisgood.sh
@@ -1,1 +1,16 @@
-#! /bin/bash
+#!/bin/bash
+
+echo -e "\033[0;32mUpdating status on github...\033[0m"
+
+cd /home/tarrafa/botaobiarra
+
+if [ "$1" == aberto ];	then
+	echo "tarrafa aberto!"
+	cp TarrafaTemGente.png status.png
+else 
+	echo "tarrafa fechado!"
+	cp TarrafaNaoTemGente.png status.png
+fi
+
+git add status.png
+git status
