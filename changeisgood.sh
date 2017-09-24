66f80c3819f5a2c638b6693e995723c4c7805c9b
tarrafabot <tarrafahc@gmail.com>
2017-09-24 15:13:27 -0300

    alterado script

--- changeisgood.sh
+++ changeisgood.sh
@@ -4,13 +4,27 @@ echo -e "\033[0;32mUpdating status on github...\033[0m"
 
 cd /home/tarrafa/botaobiarra
 
+eval "$(ssh-agent -s)"
+ssh-add botaobiarra
+
 if [ "$1" == aberto ];	then
 	echo "tarrafa aberto!"
-	cp TarrafaTemGente.png status.png
+	cp -v TarrafaTemGente.png status.png
 else 
 	echo "tarrafa fechado!"
-	cp TarrafaNaoTemGente.png status.png
+	cp -v TarrafaNaoTemGente.png status.png
 fi
 
 git add status.png
 git status
+
+# Commit changes.
+if [ $# -eq 1 ]
+  then msg="[`date`] tarrafa $1"
+else
+  msg="arguments not clear"
+fi
+
+git commit -m "$msg"
+
+git push origin master
